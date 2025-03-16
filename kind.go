e38752e38904d57a1cf69ec4d34a0ffcc6109fad
bigspawn <bigspawn@yandex.ru>
2025-03-16 13:58:11 +0100

    update version, breaking changes - removed fields
    - ArtworkUrl30
    - CollectionArtistId
    - CollectionArtistName
    - CollectionArtistViewUrl
    - CollectionHdPrice
    - ContentAdvisoryRating
    - HasITunesExtras
    - IsStreamable
    - LongDescription
    - ReleaseDate (также имеет тип time.Time, а не string)
    - ShortDescription
    - TrackHdPrice
    - TrackHdRentalPrice
    - TrackRentalPrice

--- kind.go
+++ kind.go
@@ -3,17 +3,17 @@ package go_itunes_api
 type Kind string
 
 const (
-	KindBook               Kind = "book"
 	KindAlbum              Kind = "album"
+	KindArtist             Kind = "artist"
+	KindBook               Kind = "book"
 	KindCoachedAudio       Kind = "coached-audio"
 	KindFeatureMovie       Kind = "feature-movie"
-	KindInteractiveBooklet Kind = "interactive- booklet"
+	KindInteractiveBooklet Kind = "interactive-booklet"
 	KindMusicVideo         Kind = "music-video"
 	KindPdf                Kind = "pdf"
 	KindPodcast            Kind = "podcast"
 	KindPodcastEpisode     Kind = "podcast-episode"
 	KindSoftwarePackage    Kind = "software-package"
 	KindSong               Kind = "song"
-	KindTvEpisode          Kind = "tv- episode"
-	KindArtist             Kind = "artist"
+	KindTvEpisode          Kind = "tv-episode"
 )
--- result.go
+++ result.go
@@ -1,7 +1,5 @@
 package go_itunes_api
 
-import "time"
-
 type Results struct {
 
 	// The number of results that match the search term.
@@ -11,133 +9,36 @@ type Results struct {
 	Results []Result `json:"results,omitempty"`
 }
 
+// Result represents a single search result.
+//
+// https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI/UnderstandingSearchResults.html#//apple_ref/doc/uid/TP40017632-CH8-SW1
 type Result struct {
-
-	// The name of the object returned by the search request.
-	WrapperType string `json:"wrapperType,omitempty"`
-
-	// The kind of content returned by the search request.
-	Kind Kind `json:"kind,omitempty"`
-
-	//
-	ArtistId int `json:"artistId,omitempty,omitempty"`
-
-	//
-	CollectionId int `json:"collectionId,omitempty,omitempty"`
-
-	//
-	TrackId int `json:"trackId,omitempty"`
-
-	// The name of the artist returned by the search request.
-	ArtistName string `json:"artistName,omitempty"`
-
-	// The name of the album, TV season, audiobook, and so on returned by the search
-	// request.
-	CollectionName string `json:"collectionName,omitempty,omitempty"`
-
-	// The name of the track, song, video, TV episode, and so on returned by the
-	// search request.
-	TrackName string `json:"trackName,omitempty"`
-
-	//
-	CollectionCensoredName string `json:"collectionCensoredName,omitempty,omitempty"`
-
-	//
-	TrackCensoredName string `json:"trackCensoredName,omitempty"`
-
-	//
-	ArtistViewUrl string `json:"artistViewUrl,omitempty,omitempty"`
-
-	//
-	CollectionViewUrl string `json:"collectionViewUrl,omitempty,omitempty"`
-
-	//
-	TrackViewUrl string `json:"trackViewUrl,omitempty"`
-
-	//
-	PreviewUrl string `json:"previewUrl,omitempty"`
-
-	//
-	ArtworkUrl30 string `json:"artworkUrl30,omitempty"`
-
-	//
-	ArtworkUrl60 string `json:"artworkUrl60,omitempty"`
-
-	//
-	ArtworkUrl100 string `json:"artworkUrl100,omitempty"`
-
-	//
-	CollectionPrice float64 `json:"collectionPrice,omitempty"`
-
-	//
-	TrackPrice float64 `json:"trackPrice,omitempty"`
-
-	//
-	ReleaseDate time.Time `json:"releaseDate,omitempty"`
-
-	//
-	CollectionExplicitness string `json:"collectionExplicitness,omitempty"`
-
-	//
-	TrackExplicitness string `json:"trackExplicitness,omitempty"`
-
-	//
-	DiscCount int `json:"discCount,omitempty,omitempty"`
-
-	//
-	DiscNumber int `json:"discNumber,omitempty,omitempty"`
-
-	//
-	TrackCount int `json:"trackCount,omitempty,omitempty"`
-
-	//
-	TrackNumber int `json:"trackNumber,omitempty,omitempty"`
-
-	//
-	TrackTimeMillis int `json:"trackTimeMillis,omitempty"`
-
-	//
-	Country string `json:"country,omitempty"`
-
-	//
-	Currency string `json:"currency,omitempty"`
-
-	//
-	PrimaryGenreName string `json:"primaryGenreName,omitempty"`
-
-	//
-	IsStreamable bool `json:"isStreamable,omitempty,omitempty"`
-
-	//
-	CollectionArtistId int `json:"collectionArtistId,omitempty,omitempty"`
-
-	//
-	CollectionArtistViewUrl string `json:"collectionArtistViewUrl,omitempty,omitempty"`
-
-	//
-	TrackRentalPrice float64 `json:"trackRentalPrice,omitempty,omitempty"`
-
-	//
-	CollectionHdPrice float64 `json:"collectionHdPrice,omitempty,omitempty"`
-
-	//
-	TrackHdPrice float64 `json:"trackHdPrice,omitempty,omitempty"`
-
-	//
-	TrackHdRentalPrice float64 `json:"trackHdRentalPrice,omitempty,omitempty"`
-
-	//
-	ContentAdvisoryRating string `json:"contentAdvisoryRating,omitempty,omitempty"`
-
-	//
-	ShortDescription string `json:"shortDescription,omitempty,omitempty"`
-
-	//
-	LongDescription string `json:"longDescription,omitempty,omitempty"`
-
-	//
-	HasITunesExtras bool `json:"hasITunesExtras,omitempty,omitempty"`
-
-	//
-	CollectionArtistName string `json:"collectionArtistName,omitempty,omitempty"`
+	ArtistID               int     `json:"artistId"`
+	ArtistName             string  `json:"artistName"`
+	ArtistViewURL          string  `json:"artistViewUrl"`
+	ArtworkURL100          string  `json:"artworkUrl100"`
+	ArtworkURL60           string  `json:"artworkUrl60"`
+	CollectionCensoredName string  `json:"collectionCensoredName"`
+	CollectionExplicitness string  `json:"collectionExplicitness"`
+	CollectionID           int     `json:"collectionId"`
+	CollectionName         string  `json:"collectionName"`
+	CollectionPrice        float64 `json:"collectionPrice"`
+	CollectionViewURL      string  `json:"collectionViewUrl"`
+	Country                string  `json:"country"`
+	Currency               string  `json:"currency"`
+	DiscCount              int     `json:"discCount"`
+	DiscNumber             int     `json:"discNumber"`
+	Kind                   Kind    `json:"kind"`
+	PreviewURL             string  `json:"previewUrl"`
+	PrimaryGenreName       string  `json:"primaryGenreName"`
+	TrackCensoredName      string  `json:"trackCensoredName"`
+	TrackCount             int     `json:"trackCount"`
+	TrackExplicitness      string  `json:"trackExplicitness"`
+	TrackID                int     `json:"trackId"`
+	TrackName              string  `json:"trackName"`
+	TrackNumber            int     `json:"trackNumber"`
+	TrackPrice             float64 `json:"trackPrice"`
+	TrackTimeMillis        int     `json:"trackTimeMillis"`
+	TrackViewURL           string  `json:"trackViewUrl"`
+	WrapperType            string  `json:"wrapperType"`
 }
